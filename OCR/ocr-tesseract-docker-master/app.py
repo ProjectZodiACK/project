f7eb488764e243a2e2a9d847cbc5111801e09836
Sean Walsh <walsh@psu.edu>
2022-12-05 00:17:51 -0500

    Commit for final

--- OCR/ocr-tesseract-docker-master/app.py
+++ OCR/ocr-tesseract-docker-master/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from flask_autoindex import AutoIndex
+from werkzeug.utils import secure_filename
 import os
+import datetime
+import time
 import sys
 from PIL import Image
 import pytesseract
 import argparse
 import cv2
+import subprocess
+import vig_decryption
+import func_timeout
+import solitaire
+import caesar
 
 __author__ = 'Rick Torzynski <ricktorzynski@gmail.com>'
 __source__ = ''
 
 app = Flask(__name__)
-UPLOAD_FOLDER = './static/uploads'
+UPLOAD_FOLDER = './static/uploads/' + datetime.datetime.now().strftime("%Y-%m-%d")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
@@ -23,7 +31,15 @@ def index():
 def about():
   return render_template("about.html")
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route("/history")
+def history():
+  return render_template("history.html")
+
+@app.route("/logs")
+def logs():
+  return render_template("logs.html")
+
+@app.route('/upload-file', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
@@ -31,10 +47,13 @@ def upload_file():
       # create a secure filename
       filename = secure_filename(f.filename)
 
-      # save file to /static/uploads
+      # create directory and save file to /static/uploads
+      if not(os.path.exists(app.config['UPLOAD_FOLDER']) and os.path.isdir(app.config['UPLOAD_FOLDER'])):
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+  
       filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
       f.save(filepath)
-      
+
       # load the example image and convert it to grayscale
       image = cv2.imread(filepath)
       gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -55,7 +74,41 @@ def upload_file():
       # remove the processed image
       os.remove(ofilename)
 
-      return render_template("uploaded.html", displaytext=text, fname=filename)
+      return render_template("uploaded.html", displaytext=text, fname=filepath)
+
+@app.route('/upload-solitaire', methods = ['GET', 'POST'])
+def upload_solitaire():
+   if request.method == 'POST':
+      s = request.form['text']
+      s = s.replace(' ','')
+
+      # create directory and save file to /static/uploads
+      if not(os.path.exists(app.config['UPLOAD_FOLDER']) and os.path.isdir(app.config['UPLOAD_FOLDER'])):
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+      
+      return render_template("uploaded.html", displaytext=solitaire.main(s))
+
+@app.route('/upload-vigenere', methods = ['GET', 'POST'])
+def upload_vigenere():
+   if request.method == 'POST':
+      f = request.form['text']
+
+      # create directory and save file to /static/uploads
+      if not(os.path.exists(app.config['UPLOAD_FOLDER']) and os.path.isdir(app.config['UPLOAD_FOLDER'])):
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+  
+      return render_template("uploaded.html", displaytext=vig_decryption.brute_force_decryption_vigenere(f, 100))
+
+@app.route('/upload-caesar', methods = ['GET', 'POST'])
+def upload_caesar():
+   if request.method == 'POST':
+      f = request.form['text']
+
+      # create directory and save file to /static/uploads
+      if not(os.path.exists(app.config['UPLOAD_FOLDER']) and os.path.isdir(app.config['UPLOAD_FOLDER'])):
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+  
+      return render_template("uploaded.html", displaytext=caesar.main(f))
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
